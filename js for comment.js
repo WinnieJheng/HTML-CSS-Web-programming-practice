9dbf571e28a1ad171c1929d503c32e8866c3aa1d
Wan-Yu Jheng <32569704+WinnieJheng@users.noreply.github.com>
2023-03-01 06:48:03 +0800

    新增作品集、Dark mode切換

--- js for comment.js
+++ js for comment.js
@@ -1,11 +1,8 @@
-//彈跳視窗
-alert("Hello! \(・∀・)/");
-
 // 明亮模式與暗黑模式
 
 'use strict'; // 收取錯誤訊息
 
-const switcher = document.querySelector('.btn'); // 取得按鈕參考
+const switcher = document.querySelector('.btn3'); // 取得按鈕參考
 
 // 為 click 事件新增接聽程式，並定義事件處理常式函式，以便在 click 事件發生時可由瀏覽器執行
 switcher.addEventListener('click', function() {
